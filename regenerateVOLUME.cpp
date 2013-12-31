0cc7003b5dc2abefcaecccb4680306d28f69c88b
nkersting <1054h34@gmail.com>
2013-12-31 15:19:17 -0500

    website files

---
+++ regenerateVOLUME.cpp
@@ -0,0 +1,501 @@
+// Run this code to completely regenerate the lines.txt, spectra.txt, and repoire.json files
+// Necessary if things got totally messed up somehow
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <list>
+#include <set>
+#include <vector>
+#include <memory>
+#include <fstream>
+#include <string.h>
+#include <math.h>
+#include <cstring>
+#include <map>
+#include <algorithm>
+#include <ctime>
+#include "dirent.h"
+#include <sys/types.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+
+
+using namespace std;
+
+const int FREQUENCY_BINS = 81;    // binning of the spectrum
+const int KEY_RANGE = 32000000;   // maximum expected key value
+
+const double MIN_OVERLAP = 1.0; 
+
+const char* DATADIR = "/home/nkersting/quantumrepoire.com/public/";
+const char* DATADIR_SPECTRUM = "/home/nkersting/quantumrepoire.com/spectra.txt";
+const char* DATADIR_LINES = "/home/nkersting/quantumrepoire.com/lines.txt";
+const char* REPOIRE_DATA = "/home/nkersting/quantumrepoire.com/data/repoire.json";
+
+//////////////////////////////////////////////
+
+/* A colour system is defined by the CIE x and y coordinates of
+   its three primary illuminants and the x and y coordinates of
+   the white point. */
+
+struct colourSystem {
+    string name;     	    	    /* Colour system name */
+    double xRed, yRed,	    	    /* Red x, y */
+           xGreen, yGreen,  	    /* Green x, y */
+           xBlue, yBlue,    	    /* Blue x, y */
+           xWhite, yWhite,  	    /* White point x, y */
+	   gamma;   	    	    /* Gamma correction for system */
+};
+
+/* White point chromaticities. */
+
+#define IlluminantC     0.3101, 0.3162	    	/* For NTSC television */
+#define IlluminantD65   0.3127, 0.3291	    	/* For EBU and SMPTE */
+#define IlluminantE 	0.33333333, 0.33333333  /* CIE equal-energy illuminant */
+
+/*  Gamma of nonlinear correction.
+
+    See Charles Poynton's ColorFAQ Item 45 and GammaFAQ Item 6 at:
+    
+       http://www.poynton.com/ColorFAQ.html
+       http://www.poynton.com/GammaFAQ.html
+ 
+*/
+
+#define GAMMA_REC709	0		/* Rec. 709 */
+
+static struct colourSystem
+                  /* Name                  xRed    yRed    xGreen  yGreen  xBlue  yBlue    White point        Gamma   */
+    NTSCsystem  =  { "NTSC",               0.67,   0.33,   0.21,   0.71,   0.14,   0.08,   IlluminantC,    GAMMA_REC709 },
+    EBUsystem   =  { "EBU (PAL/SECAM)",    0.64,   0.33,   0.29,   0.60,   0.15,   0.06,   IlluminantD65,  GAMMA_REC709 },
+    SMPTEsystem =  { "SMPTE",              0.630,  0.340,  0.310,  0.595,  0.155,  0.070,  IlluminantD65,  GAMMA_REC709 },
+    HDTVsystem  =  { "HDTV",               0.670,  0.330,  0.210,  0.710,  0.150,  0.060,  IlluminantD65,  GAMMA_REC709 },
+    CIEsystem   =  { "CIE",                0.7355, 0.2645, 0.2658, 0.7243, 0.1669, 0.0085, IlluminantE,    GAMMA_REC709 },
+    Rec709system = { "CIE REC 709",        0.64,   0.33,   0.30,   0.60,   0.15,   0.06,   IlluminantD65,  GAMMA_REC709 };
+
+
+//////////////////////////////////
+void SpectrumToXYZ(int spectrum[],
+                     double& x, double& y, double& z)
+{
+    int i;
+    double lambda, X = 0, Y = 0, Z = 0, XYZ;
+
+    /* CIE colour matching functions xBar, yBar, and zBar for
+       wavelengths from 380 through 780 nanometers, every 5
+       nanometers.  For a wavelength lambda in this range:
+
+            cie_colour_match[(lambda - 380) / 5][0] = xBar
+            cie_colour_match[(lambda - 380) / 5][1] = yBar
+            cie_colour_match[(lambda - 380) / 5][2] = zBar
+
+	To save memory, this table can be declared as floats
+	rather than doubles; (IEEE) float has enough 
+	significant bits to represent the values. It's declared
+	as a double here to avoid warnings about "conversion
+	between floating-point types" from certain persnickety
+	compilers. */
+
+    static double cie_colour_match[81][3] = {
+        {0.0014,0.0000,0.0065}, {0.0022,0.0001,0.0105}, {0.0042,0.0001,0.0201},
+        {0.0076,0.0002,0.0362}, {0.0143,0.0004,0.0679}, {0.0232,0.0006,0.1102},
+        {0.0435,0.0012,0.2074}, {0.0776,0.0022,0.3713}, {0.1344,0.0040,0.6456},
+        {0.2148,0.0073,1.0391}, {0.2839,0.0116,1.3856}, {0.3285,0.0168,1.6230},
+        {0.3483,0.0230,1.7471}, {0.3481,0.0298,1.7826}, {0.3362,0.0380,1.7721},
+        {0.3187,0.0480,1.7441}, {0.2908,0.0600,1.6692}, {0.2511,0.0739,1.5281},
+        {0.1954,0.0910,1.2876}, {0.1421,0.1126,1.0419}, {0.0956,0.1390,0.8130},
+        {0.0580,0.1693,0.6162}, {0.0320,0.2080,0.4652}, {0.0147,0.2586,0.3533},
+        {0.0049,0.3230,0.2720}, {0.0024,0.4073,0.2123}, {0.0093,0.5030,0.1582},
+        {0.0291,0.6082,0.1117}, {0.0633,0.7100,0.0782}, {0.1096,0.7932,0.0573},
+        {0.1655,0.8620,0.0422}, {0.2257,0.9149,0.0298}, {0.2904,0.9540,0.0203},
+        {0.3597,0.9803,0.0134}, {0.4334,0.9950,0.0087}, {0.5121,1.0000,0.0057},
+        {0.5945,0.9950,0.0039}, {0.6784,0.9786,0.0027}, {0.7621,0.9520,0.0021},
+        {0.8425,0.9154,0.0018}, {0.9163,0.8700,0.0017}, {0.9786,0.8163,0.0014},
+        {1.0263,0.7570,0.0011}, {1.0567,0.6949,0.0010}, {1.0622,0.6310,0.0008},
+        {1.0456,0.5668,0.0006}, {1.0026,0.5030,0.0003}, {0.9384,0.4412,0.0002},
+        {0.8544,0.3810,0.0002}, {0.7514,0.3210,0.0001}, {0.6424,0.2650,0.0000},
+        {0.5419,0.2170,0.0000}, {0.4479,0.1750,0.0000}, {0.3608,0.1382,0.0000},
+        {0.2835,0.1070,0.0000}, {0.2187,0.0816,0.0000}, {0.1649,0.0610,0.0000},
+        {0.1212,0.0446,0.0000}, {0.0874,0.0320,0.0000}, {0.0636,0.0232,0.0000},
+        {0.0468,0.0170,0.0000}, {0.0329,0.0119,0.0000}, {0.0227,0.0082,0.0000},
+        {0.0158,0.0057,0.0000}, {0.0114,0.0041,0.0000}, {0.0081,0.0029,0.0000},
+        {0.0058,0.0021,0.0000}, {0.0041,0.0015,0.0000}, {0.0029,0.0010,0.0000},
+        {0.0020,0.0007,0.0000}, {0.0014,0.0005,0.0000}, {0.0010,0.0004,0.0000},
+        {0.0007,0.0002,0.0000}, {0.0005,0.0002,0.0000}, {0.0003,0.0001,0.0000},
+        {0.0002,0.0001,0.0000}, {0.0002,0.0001,0.0000}, {0.0001,0.0000,0.0000},
+        {0.0001,0.0000,0.0000}, {0.0001,0.0000,0.0000}, {0.0000,0.0000,0.0000}
+    };
+
+    for (i = 0, lambda = 380; lambda < 780.1; i++, lambda += 5)
+    {
+        X += spectrum[i] * cie_colour_match[i][0];
+        Y += spectrum[i] * cie_colour_match[i][1];
+        Z += spectrum[i] * cie_colour_match[i][2];
+    }
+    XYZ = (X + Y + Z);
+    x = X / XYZ;
+    y = Y / XYZ;
+    z = Z / XYZ;
+}
+
+///////////////////////////////////////////
+
+/*                             XYZ_TO_RGB
+
+    Given an additive tricolour system CS, defined by the CIE x
+    and y chromaticities of its three primaries (z is derived
+    trivially as 1-(x+y)), and a desired chromaticity (XC, YC,
+    ZC) in CIE space, determine the contribution of each
+    primary in a linear combination which sums to the desired
+    chromaticity.  If the  requested chromaticity falls outside
+    the Maxwell  triangle (colour gamut) formed by the three
+    primaries, one of the r, g, or b weights will be negative. 
+
+    Caller can use Constrain_rgb() to desaturate an
+    outside-gamut colour to the closest representation within
+    the available gamut and/or Norm_rgb to normalise the RGB
+    components so the largest nonzero component has value 1.
+    
+*/
+
+void XYZ_to_rgb(struct colourSystem *cs,
+                double xc, double yc, double zc,
+                double *r, double *g, double *b)
+{
+    double xr, yr, zr, xg, yg, zg, xb, yb, zb;
+    double xw, yw, zw;
+    double rx, ry, rz, gx, gy, gz, bx, by, bz;
+    double rw, gw, bw;
+
+    xr = cs->xRed;    yr = cs->yRed;    zr = 1 - (xr + yr);
+    xg = cs->xGreen;  yg = cs->yGreen;  zg = 1 - (xg + yg);
+    xb = cs->xBlue;   yb = cs->yBlue;   zb = 1 - (xb + yb);
+
+    xw = cs->xWhite;  yw = cs->yWhite;  zw = 1 - (xw + yw);
+
+    /* xyz -> rgb matrix, before scaling to white. */
+    
+    rx = (yg * zb) - (yb * zg);  ry = (xb * zg) - (xg * zb);  rz = (xg * yb) - (xb * yg);
+    gx = (yb * zr) - (yr * zb);  gy = (xr * zb) - (xb * zr);  gz = (xb * yr) - (xr * yb);
+    bx = (yr * zg) - (yg * zr);  by = (xg * zr) - (xr * zg);  bz = (xr * yg) - (xg * yr);
+
+    /* White scaling factors.
+       Dividing by yw scales the white luminance to unity, as conventional. */
+       
+    rw = ((rx * xw) + (ry * yw) + (rz * zw)) / yw;
+    gw = ((gx * xw) + (gy * yw) + (gz * zw)) / yw;
+    bw = ((bx * xw) + (by * yw) + (bz * zw)) / yw;
+
+    /* xyz -> rgb matrix, correctly scaled to white. */
+    
+    rx = rx / rw;  ry = ry / rw;  rz = rz / rw;
+    gx = gx / gw;  gy = gy / gw;  gz = gz / gw;
+    bx = bx / bw;  by = by / bw;  bz = bz / bw;
+
+    /* rgb of the desired point */
+    
+    *r = (rx * xc) + (ry * yc) + (rz * zc);
+    *g = (gx * xc) + (gy * yc) + (gz * zc);
+    *b = (bx * xc) + (by * yc) + (bz * zc);
+}
+////////////////////////////////////////
+/*  	    	    	    NORM_RGB
+
+    Normalise RGB components so the most intense (unless all
+    are zero) has a value of 1.
+    
+*/
+
+void Norm_rgb(double *r, double *g, double *b)
+{
+#define Max(a, b)   (((a) > (b)) ? (a) : (b))
+    double greatest = Max(*r, Max(*g, *b));
+    
+    if (greatest > 0) {
+    	*r /= greatest;
+	*g /= greatest;
+	*b /= greatest;
+    }
+#undef Max
+}
+////////////////////////////////////
+
+/*                          CONSTRAIN_RGB
+
+    If the requested RGB shade contains a negative weight for
+    one of the primaries, it lies outside the colour gamut 
+    accessible from the given triple of primaries.  Desaturate
+    it by adding white, equal quantities of R, G, and B, enough
+    to make RGB all positive.  The function returns 1 if the
+    components were modified, zero otherwise.
+    
+*/
+
+int Constrain_rgb(double *r, double *g, double *b)
+{
+    double w;
+
+    /* Amount of white needed is w = - min(0, *r, *g, *b) */
+    
+    w = (0 < *r) ? 0 : *r;
+    w = (w < *g) ? w : *g;
+    w = (w < *b) ? w : *b;
+    w = -w;
+
+    /* Add just enough white to make r, g, b all positive. */
+    
+    if (w > 0) {
+        *r += w;  *g += w; *b += w;
+        return 1;                     /* Colour modified to fit RGB gamut */
+    }
+
+    return 0;                         /* Colour within RGB gamut */
+}
+
+
+////////////////////////
+struct File
+{
+   string name;
+   time_t modtime;
+   File (string n, time_t c)
+      {
+         name = n;
+         modtime = c;
+      }
+};
+
+struct FileCompare
+{
+   bool operator()(const File& f1, const File& f2)
+      {
+         return difftime(f1.modtime, f2.modtime) < 0;  
+      }
+};
+
+
+//////////////////////////////////////////////
+
+void ReadFilesIntoMap(map<File, set<int>, FileCompare >& keyLists)
+{
+  DIR *pDIR;
+  struct stat buff;
+  struct dirent *entry;
+  set<File, FileCompare> filestats;
+
+  // first sort the files in time-order
+  if ( pDIR=opendir(DATADIR) )
+    {
+       while (entry = readdir(pDIR))
+	{
+	  if( strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0 )
+	    {
+               string filepath = DATADIR;                  
+               filepath.append(entry->d_name);  
+               stat(filepath.c_str(), &buff);
+               filestats.insert(File(entry->d_name, buff.st_mtime));
+            }
+        }
+    }
+  closedir(pDIR);
+
+  for (set<File, FileCompare>::iterator it = filestats.begin(); it != filestats.end(); ++it)
+  {
+     cout << it->name << " " << it->modtime << endl;
+     ifstream keyfile;
+     string filepath = DATADIR;
+     filepath.append(it->name);
+     keyfile.open(filepath.c_str());
+     set <int> keylist;
+     int keyvalue;
+     while (!keyfile.eof())
+     {
+        keyfile >> keyvalue;
+        keylist.insert(keyvalue);
+     }
+     keyfile.close();
+     keyLists.insert(pair<File, set<int> >(*it, keylist));
+  }
+
+  return;
+}
+
+/////////////////////////////
+double FindMaxOverlap(const set<int>& keyListA, const set<int>& keyListB)
+{
+
+  set<int> commonKeys;
+  commonKeys.clear();
+
+  if (keyListA.size() <= keyListB.size())
+  {
+     for (set<int>::const_iterator it = keyListA.begin(); it!= keyListA.end(); it++)
+     {
+        if (keyListB.find(*it) !=  keyListB.end()) commonKeys.insert(*it);
+     }
+     return (double)(100.0*commonKeys.size()/keyListA.size());
+  }
+  else
+  {
+     for (set<int>::const_iterator it = keyListB.begin(); it!= keyListB.end(); it++)
+     {
+        if (keyListA.find(*it) !=  keyListA.end()) commonKeys.insert(*it);
+     }
+     return (double)(100.0*commonKeys.size()/keyListB.size());
+  }
+}
+
+/////////////////////////////////////////////
+
+void  WriteRepoireJSON()
+{  
+  // open output file
+  ofstream outfile;
+  outfile.open(REPOIRE_DATA);   
+
+  outfile << "{ \"nodes\":[" << endl;
+
+  ifstream specfile;
+  specfile.open(DATADIR_SPECTRUM);
+  vector <string> filenames;
+  if (specfile.is_open()) 
+  {
+     int count = 0;
+     double x,y;
+     int r,g,b;
+     int numkeys;
+     string contribution;
+
+     while ( !specfile.eof())
+     {
+        count++;
+        specfile >> contribution >> numkeys >> x >> y >> r >> g >> b;
+        filenames.push_back(contribution);
+        outfile << "{" <<
+	"\"match\": \"" << 1.0  << "\"," << endl <<
+           "\"name\": \"" << contribution.substr(0, contribution.size()-4)  << "\"," << endl <<
+	"\"artist\": \"" << numkeys  << "\"," << endl <<
+             "\"id\": \"" << contribution.substr(0, contribution.size()-4)  << "\"," << endl <<
+	"\"playcount\":" << numkeys << "," << endl << 
+	"\"CIEx\":" << x << "," << endl << 
+	"\"CIEy\":" << y << "," << endl << 
+	"\"red\":" << r << "," << endl << 
+	"\"blue\":" << g << "," << endl << 
+	"\"green\":" << b <<  "}" << endl ; 
+        if (!specfile.eof()) outfile << "," << endl;
+     }
+  }
+  specfile.close();
+  outfile << "], \"links\":["; 
+  ifstream infile;
+  infile.open(DATADIR_LINES);
+
+  double x1,y1,x2,y2,overlap;
+  int xcount, ycount;
+  while (!infile.eof())
+  {
+
+     infile >> x1 >> y1 >> x2 >> y2 >> overlap >> xcount >> ycount;
+
+     outfile << "{" << endl <<
+        "\"source\": \"" << (filenames.at(xcount - 1)).substr(0, (filenames.at(xcount - 1)).size()-4) << "\"," << endl << 
+        "\"target\": \"" << (filenames.at(ycount - 1)).substr(0, (filenames.at(ycount - 1)).size()-4) << "\"," << endl << 
+        "\"overlap\": " << overlap/100.0 << endl << "}" << endl;
+     if (!infile.eof()) outfile << "," << endl;
+  }
+
+  outfile << "]  }";
+  outfile.close();
+}
+/////////////////////////
+void ComputeSpectrum(double& x, double& y, double& z, set<int> key_set)
+{
+   int spectrum[FREQUENCY_BINS];
+   for (int i = 0; i < FREQUENCY_BINS; i++)
+   {
+      spectrum[i] = 0;
+   }
+
+   for (set<int>::iterator it = key_set.begin();
+        it != key_set.end(); ++it) 
+   {
+      int key = *it;
+      if (key > 0 && key < KEY_RANGE)
+      {
+         spectrum[(int)((1.0*key/KEY_RANGE)*FREQUENCY_BINS)]++;
+      }
+      else
+      {
+         spectrum[(int)(log(abs(key)))]++;    // keys from non-dict words are collected in the lowest bins
+      }
+   }
+   SpectrumToXYZ(spectrum, x, y, z);
+}
+
+////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+int main(int argc, char* argv[])
+{
+
+   map<File, set<int>, FileCompare > keyLists;
+   ReadFilesIntoMap(keyLists);    
+
+   //set up to make "spectrum.txt"
+   fstream spectrum_file;
+   spectrum_file.open (DATADIR_SPECTRUM, fstream::out | fstream::trunc);
+
+   //set up to make "lines.txt" 
+   fstream line_file;
+   line_file.open(DATADIR_LINES, fstream::out | fstream::trunc);
+
+
+   int spectrum[FREQUENCY_BINS];           
+   int xcount = 0;
+   int ycount = 0;
+
+   for (map<File, set<int>, FileCompare >::iterator itx = keyLists.begin();   // iterate over files
+        itx != keyLists.end(); ++itx)
+   {
+      ++xcount;
+
+      struct colourSystem *cs = &SMPTEsystem;
+      double x1,y1,z1,r,g,b;
+      ComputeSpectrum(x1, y1, z1, itx->second);
+      XYZ_to_rgb(cs, x1, y1, z1, &r, &g, &b);                     
+      Constrain_rgb(&r, &g, &b);
+      Norm_rgb(&r, &g, &b);      
+  
+      spectrum_file << itx->first.name << " " << itx->second.size() << " " << x1 << " " << y1 << " " 
+               << (int)(255*r) << " " << (int)(255*g) << " " << (int)(255*b) << '\n';
+
+      ycount = 0;
+      for (map<File, set<int>, FileCompare >::iterator ity = keyLists.begin();      
+           ity != keyLists.end(); ++ity)                
+      { 
+         ++ycount;
+         if (xcount >= ycount) { continue;}  // only consider unique pairs
+
+         double x2,y2,z2;
+         ComputeSpectrum(x2, y2, z2, ity->second);
+
+         // now compare the two files to get percent overlap
+         double overlap = FindMaxOverlap(itx->second, ity->second);
+         
+         if (overlap > MIN_OVERLAP)    // arbitrary limit
+         {
+            line_file << x1 << " " << y1 << " " << x2 << " " << y2 << " " 
+                    << overlap << " " << xcount << " " << ycount << endl;
+         }
+      }
+   }
+
+   line_file.close();
+   spectrum_file.close();
+   WriteRepoireJSON();    // Write the JSON file used for the force network
+   return 0;
+}
+
+
---
+++ simple_encrypt.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+#  BEFORE RUNNING: make sure this script and "omega-hybrid.txt" are in the same directory.
+#  USAGE:  Simply run this script and, when prompted, enter the message you wish to encrypt.
+#  OUTPUT: File of the form "diy_keys+<timestamp>.kyx". Feel free to rename this file to your tastes, but please
+#          keep the *.kyx extension.
+#  UPLOAD: You are encouraged to upload the output file to the Quantum Repoire Server (http://www.quantumrepoire.com)
+#######################################################################
+#########################################################################
+# This script produces usable sum-encrypted messages, but there are still some
+# additional needed improvements. For example,
+# 1. handle punctuation (note convert "read." to "read" but not "U.S." to "U.S")
+# 2. hash words not in the dictionary to unused unique integers 
+# 3. convert plural nouns to singular (WordNet has no plurals)
+########################################################################################
+
+import csv
+import sys
+import shlex
+import time
+
+#########################################
+
+def uniq(list):
+    set = {}
+    return [set.setdefault(x,x) for x in list if x not in set]
+
+##########################
+def findlist(entryword, syndict, stopwords):
+    newlist = []
+    if (syndict.has_key(entryword) and entryword not in stopwords):
+        return syndict[entryword]
+    else:
+        return newlist
+
+##########################################
+stopwords = ['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', 'aren\'t', 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', 'can\'t', 'cannot', 'could', 'couldn\'t', 'did', 'didn\'t', 'do', 'does', 'doesn\'t', 'doing', 'don\'t', 'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', 'hadn\'t', 'has', 'hasn\'t', 'have', 'haven\'t', 'having', 'he', 'he\'d', 'he\'ll', 'he\'s', 'her', 'here', 'here\'s', 'hers', 'herself', 'him', 'himself', 'his', 'how', 'how\'s', 'i', 'i\'d', 'i\'ll', 'i\'m', 'i\'ve', 'if', 'in', 'into', 'is', 'isn\'t', 'it', 'it\'s', 'its', 'itself', 'let\'s', 'me', 'more', 'most', 'mustn\'t', 'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours', 'ourselves', 'out', 'over', 'own', 'same', 'shan\'t', 'she', 'she\'d', 'she\'ll', 'she\'s', 'should', 'shouldn\'t', 'so', 'some', 'such', 'than', 'that', 'that\'s', 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', 'there\'s', 'these', 'they', 'they\'d', 'they\'ll', 'they\'re', 'they\'ve', 'this', 'those', 'through', 'to', 'too', 'under', 'until', 'up', 'very', 'was', 'wasn\'t', 'we', 'we\'d', 'we\'ll', 'we\'re', 'we\'ve', 'were', 'weren\'t', 'what', 'what\'s', 'when', 'when\'s', 'where', 'where\'s', 'which', 'while', 'who', 'who\'s', 'whom', 'why', 'why\'s', 'with', 'won\'t', 'would', 'wouldn\'t', 'you', 'you\'d', 'you\'ll', 'you\'re', 'you\'ve', 'your', 'yours', 'yourself', 'yourselves']
+
+synfile = "omega-hybrid.txt"    
+
+saveplace = "diy_keys" + time.strftime("%Y_%m_%d_%H_%M_%S_%P") + ".kyx"
+outfile = open(saveplace, "w")
+
+synReader = csv.reader(open(synfile,'rb'), delimiter=' ')
+
+syns = []
+for synline in synReader:
+    syns.append(synline)
+
+syndict = {}
+for test in syns:
+    if syndict.has_key(test[0]) == False:
+        templist = []
+        for j in range (1, len(test)):
+            if (test[j] != ''):
+                templist.append(test[j])
+        syndict[test[0]] = templist
+            
+usertext = raw_input('Your text (hit Enter to quit): ')
+
+if usertext == '':
+    sys.exit()                ## quit the program
+
+userwords = shlex.split(usertext)
+
+sumlist = []
+for i in range(0,len(userwords)):        # all possible word pairs considered
+    for j in range(i+1, len(userwords)):  
+        word1 = userwords[i]
+        word2 = userwords[j]
+        if word1 == word2:
+            continue
+                     
+        total1 = findlist(word1.lower(), syndict, stopwords)  # find the set of synsets associated with this word
+        total2 = findlist(word2.lower(), syndict, stopwords)
+           
+        for w1 in total1:         # take the Cartesian product of the two sets
+            for w2 in total2:
+                sumlist.append(int(w1)+int(w2)) # this is the heart of the secure algorithm;
+                                                # the sum retains information about the individual synsets,
+                                                # but is impossible to resolve back into its constituents
+
+for key in sorted(uniq(sumlist)):              # write output with one sum per line
+    outfile.write(str(key) + '\n')
